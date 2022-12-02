c02a78bd42700d5caeb56c148d2e15e8e866aba1
Axel Olausson Holtenas <vermium@zyner.org>
2022-12-02 21:15:36 +0100

    Mohahahhaha

--- home-manager/polybar.nix
+++ home-manager/polybar.nix
@@ -1,27 +1,36 @@
 { config, pkgs, ... }:
 
-let
-  mypolybar = pkgs.polybar.override {
-    alsaSupport = true;
-    githubSupport = true;
-    mpdSupport = true;
-    pulseSupport = true;
-  };
-
-in
-{
   services.polybar = {
     enable = true;
-    package = mypolybar;
-    config = ''
-[bar/main]
-width = 100%
-height = 48
-radius = 6.0
-fixed-center = true
-'';
+    package = pkgs.polybar.override {
+      i3Support = true;
+      alsaSupport = true;
+      iwSupport = true;
+      githubSupport = true;
+    };
+    config = {
+      "bar/top" = {
+        monitor = "eDP1";
+        width = "100%";
+        height = "3%";
+        radius = 0;
+        # Just sticking them together in the center for now
+        modules-center = "date i3";
+      };
+      "module/date" = {
+        type = "internal/date";
+        internal = 5;
+        date = "%Y-%m-%d";
+        time = "%H:%M";
+        label = "%date% %time%";
+      };
+      "module/i3" = {
+        type = "internal/i3";
+        scroll-up = "i3wm-wsnext";
+        scroll-down = "i3wm-wsprev";
+      };
+    };
     script = ''
-polybar main &
-'';
+    polybar top &
+    '';
   };
-}
