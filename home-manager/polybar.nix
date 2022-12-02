8c1142062a93c6fc5345a991336b68c93fee8034
Axel Olausson Holtenas <vermium@zyner.org>
2022-12-02 21:14:32 +0100

    whopsidopsi

--- home-manager/polybar.nix
+++ home-manager/polybar.nix
@@ -13,5 +13,15 @@ in
   services.polybar = {
     enable = true;
     package = mypolybar;
+    config = ''
+[bar/main]
+width = 100%
+height = 48
+radius = 6.0
+fixed-center = true
+'';
+    script = ''
+polybar main &
+'';
   };
 }
