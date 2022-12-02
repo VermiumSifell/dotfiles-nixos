98b5782a1379e39da8ded2e5adaa55c55e60648b
Axel Olausson Holtenas <vermium@zyner.org>
2022-12-02 20:48:05 +0100

    Set keyboard layout to se

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -86,6 +86,8 @@
     ];
   };
 
+  services.xserver.layout = "se";
+
   # Enable the Plasma 5 Desktop Environment.
   # services.xserver.displayManager.sddm.enable = true;
   # services.xserver.desktopManager.plasma5.enable = true;
