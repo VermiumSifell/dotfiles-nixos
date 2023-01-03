178b4e6029f4424a9dc625405d3c1654e3b3cc66
Axel Olausson Holtenas <vermium@zyner.org>
2023-01-03 22:08:19 +0100

    test

--- home-manager/services/polybar/default.nix
+++ home-manager/services/polybar/default.nix
@@ -14,6 +14,22 @@ in
       githubSupport = true;
     };
     config = ./config.ini;
+    extraConfig = [
+      ./modules/battery.ini
+      ./modules/bspwm.ini
+      ./modules/cpu.ini
+      ./modules/date.ini
+      ./modules/eth.ini
+      ./modules/filesystem.ini
+      ./modules/i3.ini
+      ./modules/memory.ini
+      ./modules/network-base.ini
+      ./modules/pulseaudio.ini
+      ./modules/wlan.ini
+      ./modules/xkeyboard.ini
+      ./modules/xwindow.ini
+      ./modules/xworkspaces.ini
+    ];
     script = ''
       polybar top &
     '';
