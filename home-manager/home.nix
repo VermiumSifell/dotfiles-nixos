5c9a76ef2640f76e993878ef4d58eca72569a91c
Axel Olausson Holtenas <vermium@zyner.org>
2023-01-03 20:38:31 +0100

    test

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,21 +1,10 @@
-# This is your home-manager configuration file
-# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
-
-{ inputs, lib, config, pkgs, ... }: {
-  # You can import other home-manager modules here
+{ config, lib, pkgs, stdenv, ... }: {
   imports = [
-    # If you want to use home-manager modules from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModule
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./nvim.nix
-    # ./polybar.nix
     ./gnupg.nix
     ./polybar.nix
     ./alacritty.nix
   ];
 
-  # TODO: Set your username
   home = {
     username = "vermium";
     homeDirectory = "/home/vermium";
@@ -23,8 +12,6 @@
 
   fonts.fontconfig.enable = true;
 
-  # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
   home.packages = with pkgs; [
     discord
     spotify
