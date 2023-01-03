b92eaa39663afb2f0510751b184e011d03c0134f
Axel Olausson Holtenas <vermium@zyner.org>
2023-01-03 19:48:36 +0100

    test

--- home-manager/alacritty.nix
+++ home-manager/alacritty.nix
@@ -63,17 +63,6 @@
 
       };
 
-      window = {
-        opacity = 0.8;
-        dynamic_title = true;
-      };
-      draw_bold_text_with_bright_colors = true;
-      bell = {
-        animation = "EaseOutExpo";
-        duration = 10.0;
-      };
-      cursor.style.blinking = "On";
-      mouse.hide_when_typing = true;
     };
   };
 }
