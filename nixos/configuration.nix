709ee3ee20b62ab8f00efd470ce254f884f0db31
Axel Olausson Holtenas <vermium@zyner.org>
2023-01-03 20:54:45 +0100

    test

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -84,6 +84,14 @@
   # services.xserver.displayManager.sddm.enable = true;
   # services.xserver.desktopManager.plasma5.enable = true;
 
+
+   programs.steam = {
+     enable = true;
+     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+   };
+
+
   users.users = {
     vermium = {
       # TODO: You can set an initial password for your user.
