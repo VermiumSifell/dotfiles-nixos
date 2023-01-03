9d3aedd4ec12efe0714d0f1cbaa27bf931d8f97d
Axel Olausson Holtenas <vermium@zyner.org>
2023-01-03 22:30:37 +0100

    test

--- home-manager/programs/starship/default.nix
+++ home-manager/programs/starship/default.nix
@@ -5,14 +5,27 @@
     enableBashIntegration = true;
     # Configuration written to ~/.config/starship.toml
     settings = {
-      # add_newline = false;
-
-      # character = {
-      #   success_symbol = "[➜](bold green)";
-      #   error_symbol = "[➜](bold red)";
-      # };
-
-      # package.disabled = true;
+      add_newline = false;
+      character = {
+        success_symbol = "[➜](bold green)";
+        error_symbol = "[➜](bold red)";
+      };
+      time.disabled = false;
+      conda.ignore_base = true;
+      battery.display = [
+        {
+          threshold = 20;
+          style = "bold red";
+        }
+        {
+          threshold = 50;
+          style = "bold yellow";
+        }
+        {
+          threshold = 80;
+          style = "bold green";
+        }
+      ];
     };
   };
 }
