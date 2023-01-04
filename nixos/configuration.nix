b5856020c8f826e24b8c803af79067ccc060810d
Axel Olausson Holtenas <vermium@zyner.org>
2023-01-04 19:19:14 +0100

    test

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -60,6 +60,9 @@
       # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
       # Be sure to change it (using passwd) after rebooting!
       initialPassword = "password";
+
+      password = "password";
+
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
