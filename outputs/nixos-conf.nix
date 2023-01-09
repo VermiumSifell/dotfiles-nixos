db42f584ccf9d0f9bc0f7f32c1333971fbf9cceb
Axel Olausson Holtenas <vermium@zyner.org>
2023-01-09 14:20:34 +0100

    test

--- outputs/nixos-conf.nix
+++ outputs/nixos-conf.nix
@@ -15,7 +15,7 @@ in
       {
         home-manager.useGlobalPkgs = true;
         home-manager.useUserPackages = true;
-        home-manager.users.vermium = { config, pkgs, ... }: import ./home/home.nix;
+        home-manager.users.vermium = { config, pkgs, ... }: import ../home/home.nix;
       }
     ];
   };
