5c008f3f2dd168096c7f3d2d938c73da02f241d8
Axel Olausson Holtenas <vermium@zyner.org>
2023-01-09 10:41:30 +0100

    test

--- flake.nix
+++ flake.nix
@@ -1,11 +1,15 @@
 {
-  description = "NixOS configuration";
+  description = "Vermium's Home Manager & NixOS configurations";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     nur.url = "github:nix-community/nur";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { nixpkgs, nur, home-manager, ... }@attrs:
@@ -25,12 +29,12 @@
 
           modules = [
             ({ config, pkgs, ... }: { nixpkgs.overlays = [ nur.overlay ]; })
-            ./nixos/configuration.nix
+            ./system/configuration.nix
             home-manager.nixosModules.home-manager
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.users.vermium = { config, pkgs, ... }: import ./home-manager/home.nix { inherit gtk-theme config pkgs; };
+              home-manager.users.vermium = { config, pkgs, ... }: import ./home/home.nix { inherit gtk-theme config pkgs; };
             }
           ];
         };
