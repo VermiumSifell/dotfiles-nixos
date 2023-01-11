57a6396365ecfa707db98e3ab7175764c62b5180
Axel Olausson Holtenas <vermium@zyner.org>
2023-01-11 14:09:24 +0100

    re-organizing build

--- machines/AxelLaptop01/home/default.nix
+++ machines/AxelLaptop01/home/default.nix
@@ -78,52 +78,52 @@ in
     # ];
 
 
-    programs.firefox = {
-      enable = true;
-      extensions = [
-        firefox-addons.packages.x86_64-linux.bitwarden
-        firefox-addons.packages.x86_64-linux.ublock-origin
-        firefox-addons.packages.x86_64-linux.kristofferhagen-nord-theme
-      ];
-      profiles = {
-        default = {
-          isDefault = true;
-          settings = {
-            # Do not save passwords to Firefox...
-            "security.ask_for_password" = false;
+    # programs.firefox = {
+    #   enable = true;
+    #   extensions = [
+    #     firefox-addons.packages.x86_64-linux.bitwarden
+    #     firefox-addons.packages.x86_64-linux.ublock-origin
+    #     firefox-addons.packages.x86_64-linux.kristofferhagen-nord-theme
+    #   ];
+    #   profiles = {
+    #     default = {
+    #       isDefault = true;
+    #       settings = {
+    #         # Do not save passwords to Firefox...
+    #         "security.ask_for_password" = false;
 
-            # We handle this elsewhere
-            "browser.shell.checkDefaultBrowser" = false;
+    #         # We handle this elsewhere
+    #         "browser.shell.checkDefaultBrowser" = false;
 
-            # Don't allow websites to prevent use of right-click, or otherwise
-            # messing with the context menu.
-            "dom.event.contextmenu.enabled" = true;
+    #         # Don't allow websites to prevent use of right-click, or otherwise
+    #         # messing with the context menu.
+    #         "dom.event.contextmenu.enabled" = true;
 
-            # Don't allow websites to prevent copy and paste. Disable
-            # notifications of copy, paste, or cut functions. Stop webpage
-            # knowing which part of the page had been selected.
-            "dom.event.clipboardevents.enabled" = true;
+    #         # Don't allow websites to prevent copy and paste. Disable
+    #         # notifications of copy, paste, or cut functions. Stop webpage
+    #         # knowing which part of the page had been selected.
+    #         "dom.event.clipboardevents.enabled" = true;
 
-            # Do not track from battery status.
-            "dom.battery.enabled" = false;
+    #         # Do not track from battery status.
+    #         "dom.battery.enabled" = false;
 
-            # Show punycode. Help protect from character 'spoofing'.
-            "network.IDN_show_punycode" = true;
+    #         # Show punycode. Help protect from character 'spoofing'.
+    #         "network.IDN_show_punycode" = true;
 
-            # Disable site reading installed plugins.
-            "plugins.enumerable_names" = "";
+    #         # Disable site reading installed plugins.
+    #         "plugins.enumerable_names" = "";
 
-            # Enable auto scrolling.
-            "general.autoScroll" = true;
+    #         # Enable auto scrolling.
+    #         "general.autoScroll" = true;
 
-            # Remove those extra empty spaces in both sides
-            "browser.uiCustomization.state" = ''
-              {"placements":{"widget-overflow-fixed-list":[],"nav-bar":["back-button","forward-button","stop-reload-button","urlbar-container","downloads-button","fxa-toolbar-menu-button"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":["developer-button"],"dirtyAreaCache":["nav-bar","PersonalToolbar"],"currentVersion":17,"newElementCount":4}
-            '';
-          };
-        };
-      };
-    };
+    #         # Remove those extra empty spaces in both sides
+    #         "browser.uiCustomization.state" = ''
+    #           {"placements":{"widget-overflow-fixed-list":[],"nav-bar":["back-button","forward-button","stop-reload-button","urlbar-container","downloads-button","fxa-toolbar-menu-button"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":["developer-button"],"dirtyAreaCache":["nav-bar","PersonalToolbar"],"currentVersion":17,"newElementCount":4}
+    #         '';
+    #       };
+    #     };
+    #   };
+    # };
 
 
 
