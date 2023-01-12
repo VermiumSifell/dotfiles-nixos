99b30e2666ad5b22258871dc42427addae83621c
Axel Olausson Holtenas <vermium@zyner.org>
2023-01-12 16:42:29 +0100

    organize system/configuration.nix

--- system/configuration.nix
+++ system/configuration.nix
@@ -1,32 +1,48 @@
-{ inputs, lib, config, pkgs, ... }: {
+{ inputs, lib, config, pkgs, ... }:
+
+{
+  # TODO: Redo this part
   imports = [
     ./hardware-configuration.nix
     ./services.nix
     ./programs.nix
   ];
 
-  networking = {
-    hostName = "AxelLaptop01";
-    networkmanager = {
-      enable = true;
+  hardware = {
+    nvidia.prime = {
+      sync.enable = true;
+
+      nvidiaBusId = "PCI:1:0:0";
+      amdgpuBusId = "PCI:5:0:0";
+    };
+
+    bluetooth.enable = true;
+
+    opengl.enable = true;
+    opengl.driSupport32Bit = true;
+  };
+
+  boot = {
+    loader = {
+      grub = {
+        enable = true;
+        efiSupport = true;
+        device = "nodev";
+      };
+
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/efi";
+      };
     };
   };
 
   time.timeZone = "Europe/Stockholm";
 
-  services.greenclip.enable=true;
-
-
-  boot.loader.grub.enable = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/efi";
-
-  # Enable i3wm.
-  environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw 
-
-  programs.zsh.enable = true;
+  networking = {
+    hostName = "AxelLaptop01";
+    networkmanager.enable = true;
+  };
 
   users.users = {
     vermium = {
@@ -36,44 +52,22 @@
     };
   };
 
-  security.rtkit.enable = true;
-
-  virtualisation.docker.enable = true;
-  virtualisation.docker.storageDriver = "btrfs";
-
-  hardware = {
-    nvidia.prime = {
-      sync.enable = true;
-
-      # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-      nvidiaBusId = "PCI:1:0:0";
-
-      # Bus ID of the AMD GPU. You can find it using lspci, either under 3D or VGA
-      amdgpuBusId = "PCI:5:0:0";
-    };
-
-    bluetooth.enable = true;
-
-    opengl.enable = true;
-    opengl.driSupport32Bit = true;
-
-  };
-
-  nixpkgs = {
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
+  virtualisation = {
+    docker = {
+      enable = true;
+      storageDriver = "btrfs";
     };
   };
 
-  nix = {
-    settings = {
-      # Enable flakes and new 'nix' command
-      experimental-features = "nix-command flakes";
-      # Deduplicate and optimize nix store
-      auto-optimise-store = true;
-    };
+  security = {
+    rtkit.enable = true;
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  nix.settings = {
+    experimental-features = "nix-command flakes";
+    auto-optimise-store = true;
   };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
