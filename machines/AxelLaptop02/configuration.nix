0c93e4aff382c9c622e1332da8f64bb01773a6be
Axel Olausson Holtenas <vermium@zyner.org>
2023-01-21 22:39:45 +0100

    test

--- machines/AxelLaptop02/configuration.nix
+++ machines/AxelLaptop02/configuration.nix
@@ -95,15 +95,6 @@
         theme = "robbyrussell";
       };
     };
-
-    steam = {
-      enable = true;
-      remotePlay.openFirewall =
-        true; # Open ports in the firewall for Steam Remote Play
-      dedicatedServer.openFirewall =
-        true; # Open ports in the firewall for Source Dedicated Server
-    };
-
   };
 
   environment = {
